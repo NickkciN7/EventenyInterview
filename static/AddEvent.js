95d2cc99ef3267623490d8a7bb54841c3c6d4b9d
Dominick DiLeo <nickkcindi@outlook.com>
2022-07-16 23:32:57 -0400

    AddEvent page js doesnt make post request unless all input tags have user input. Upon entry of letter in zipcode input, a red error message will appear saying no letters and delete the letter. test value sent with post request in JS successfully read in add.php, still need to send all input variables then make entry in database

--- static/AddEvent.js
+++ static/AddEvent.js
@@ -1,6 +1,7 @@
 window.onload = (event) => {
 
-    stateselect = document.getElementById('stateselect');
+    // So I didn't have to manually make an option for every state in the select tag. copied this array from https://usastatescode.com/state-array-json
+    stateselect = document.getElementById('stateSelect');
     stateArray = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado',
         'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois',
         'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland',
@@ -16,9 +17,81 @@ window.onload = (event) => {
         opt.innerHTML = stateArray[i];
         stateselect.appendChild(opt);
     }
+
 };
 
 function zip() {
-    zipcode = document.getElementById('zip');
+    zipCodeElement = document.getElementById('zipInput');
+    zipCode = zipCodeElement.value;
+    lastLetter = zipCode.charAt(zipCode.length - 1);
+    //isn't a number
+    if (!(lastLetter >= '0' && lastLetter <= '9') & zipCode != '') {
+        document.getElementById('zipSpan').innerHTML = "Can Only Contain Numbers"; //warning message
+        //delete the last character
+        zipCodeElement.value = zipCode.slice(0, -1);
+    } else {
+        document.getElementById('zipSpan').innerHTML = "";
+    }
+}
 
+function add() {
+
+
+    empty = [];
+    eventName = document.getElementById('name').value;
+    if (isEmpty(eventName)) { empty.push('Name'); }
+
+    address = document.getElementById('address').value;
+    if (isEmpty(address)) { empty.push('Address'); }
+    city = document.getElementById('city').value;
+    if (isEmpty(city)) { empty.push('City'); }
+    state = document.getElementById('stateSelect').value;
+    if (state === '--Select A State--') { empty.push('State'); }
+    zipCode = document.getElementById('zipInput').value;
+    if (isEmpty(zipCode)) { empty.push('Zip Code'); }
+
+    start = document.getElementById('start').value;
+    if (isEmpty(start)) { empty.push('Start Time'); }
+    end = document.getElementById('end').value;
+    if (isEmpty(end)) { empty.push('End Time'); }
+
+    description = document.getElementById('description').value;
+    if (isEmpty(description)) { empty.push('Description'); }
+
+    // console.log(empty);
+
+    if (empty.length == 0) {
+        //nothing is empty so make a Post request
+
+        //apparently need a FormData variable to be able to use $_POST[] according to stackoverflow posts
+        const eventInfo = new FormData();
+        eventInfo.set('name', eventName);
+
+        fetch('add.php', {
+            method: 'POST',
+            body: eventInfo,
+        }).then(response => response.json()).then(data => {
+            console.log(data);
+        });
+    } else {
+        //make comma separated string from empty array and make alert notifying user what needs input
+
+        emptyString = '';
+        for (i = 0; i < empty.length; i++) {
+            emptyString += empty[i] + ', ';
+        }
+        //get rid of last comma and space
+        emptyString = emptyString.slice(0, -2);
+        // console.log(emptyString);
+        alert("Please provide input for the following: \n" + emptyString);
+    }
+
+
+
+
+}
+
+//checks if string is null or has length of 0.
+function isEmpty(str) {
+    return (!str || str.length === 0);
 }
